537027ea4121a733afbcf72383877507b729057a
tanmayi-manjunath <77391322+tanmayi-manjunath@users.noreply.github.com>
2021-05-28 19:19:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject	
+var world;
+var paper1;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+
+	Engine.run(engine);
+  
+	paper1 = new paper(180,450,75);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+
+  groundObject.display();
+  dustbinObj.display();
+  paper1.display();
+
+}
+
+function keyPressed(){
+	if(keyCode === UP_ARROW) {
+	  Matter.Body.applyForce(paper1.body, paper1.body.position, {x:130, y: -145})
+	}
+  }
+
